72f20195031f832917447c6cae2d96b9e7d337af
shawqun <xxxxiaoqun1995@163.com>
2020-06-05 14:40:58 +0800

    测试

--- HelloProjet/src/Main.java
+++ HelloProjet/src/Main.java
@@ -5,11 +5,6 @@ public class Main {
     @Test
     public void testHelloWrold() {
         System.out.println("HelloWorld!");
-	//sdafdsfa
-<<<<<<< HEAD
-	String s = "abc";
-=======
-	int i= 1+2;
->>>>>>> 82299eae3617d6a8d9e0537c9441d9309fe2ee26
+//abc
     }
 }
